5cc4577342393ac0cd261b912bf995b40b5da058
based <basedpositiveforreal@gmail.com>
2024-11-27 18:59:05 +0500

    bottomnav

--- app/src/main/java/com/example/sakr/MainSreen.kt
+++ app/src/main/java/com/example/sakr/MainSreen.kt
@@ -10,12 +10,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
